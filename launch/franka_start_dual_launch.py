d7a06b1cd068556707928aed9b7bf939e677696c
qlin960618 <qlin1806@gmail.com>
2025-05-13 13:51:05 +0900

    added dual arm unlock

---
+++ launch/franka_start_dual_launch.py
@@ -0,0 +1,42 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+from launch.substitutions import TextSubstitution, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    hostname_list = [
+        "172.16.0.2",
+        "172.16.0.3",
+    ]
+    name_list = [
+        "unlock_arm0",
+        "unlock_arm1",
+    ]
+    username = "moonshot"
+    password = "moonshotfranka"
+    launch_srcs = []
+    for hostname, name in zip(hostname_list, name_list):
+        launch_srcs.append(
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    PathJoinSubstitution([
+                        FindPackageShare('franka_lock_unlock'),
+                        'launch',
+                        'franka_start_single_launch.py'
+                    ])
+                ),
+                launch_arguments={
+                    'hostname': hostname,
+                    'name': name,
+                    'username': username,
+                    'password': password
+                }.items(),
+            )
+        )
+
+
+    return LaunchDescription([
+        *launch_srcs
+    ])
